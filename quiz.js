058161f9a57c7eacbeb3b5d3d87cdbba50b621e8
Okindo <okindo.dev@gmail.com>
2025-08-08 11:23:39 +0300

    Simple quiz project

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer(){
+    const correctAnswer = "4";
+   
+    const feedback = document.getElementById('feedback')
+    const userAnswer = document.querySelector("input[name=quiz]:checked").value
+    if (userAnswer===correctAnswer){
+        feedback.textContent = "Correct! Well done."
+    }
+    else {
+        feedback.textContent = "That's incorrect. Try again!"
+
+    }
+    
+}
+const submit = document.getElementById("submit-answer");
+submit.addEventListener("click",checkAnswer);
